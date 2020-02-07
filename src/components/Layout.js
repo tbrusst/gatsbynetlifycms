c709880fe0d34f1662316f8b2243e4ac0378f4c5
BrendanRusso <russobrendanm@gmail.com>
2020-02-07 15:39:44 -0500

    hahahaha

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -34,13 +34,13 @@ const TemplateWrapper = ({ children }) => {
         <link
           rel="icon"
           type="image/png"
-          href={`${withPrefix("/")}img/favicon-32x32 copy.png`}
+          href={`${withPrefix("/")}img/favicon-32x32-copy.png`}
           sizes="32x32"
         />
         <link
           rel="icon"
           type="image/png"
-          href={`${withPrefix("/")}img/favicon-16x16 copy.png`}
+          href={`${withPrefix("/")}img/favicon-16x16-copy.png`}
           sizes="16x16"
         />
 
