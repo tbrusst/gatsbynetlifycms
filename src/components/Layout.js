752bc43732e57772d056b7136ed665e6da1c30e5
BrendanRusso <russobrendanm@gmail.com>
2020-02-07 15:31:10 -0500

    yolo

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -34,13 +34,13 @@ const TemplateWrapper = ({ children }) => {
         <link
           rel="icon"
           type="image/png"
-          href={`${withPrefix("/")}img/favicon-32.png`}
+          href={`${withPrefix("/")}img/favicon-32x32.png`}
           sizes="32x32"
         />
         <link
           rel="icon"
           type="image/png"
-          href={`${withPrefix("/")}img/favicon-16.png`}
+          href={`${withPrefix("/")}img/favicon-16x16.png`}
           sizes="16x16"
         />
 
