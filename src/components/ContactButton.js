f8477fc2177074eb3d58c97722e1c13a4f4c5633
BrendanRusso <russobrendanm@gmail.com>
2020-07-09 01:17:54 -0400

    colors and shit uh oh

--- src/components/ContactButton.js
+++ src/components/ContactButton.js
@@ -8,9 +8,9 @@ import { theme } from "../styles/GlobalStyle";
 const ContactButton = class extends React.Component {
   render() {
     return (
-      <a href="mailto:russobrendanm@gmail.com">
+      <a id="button-link" href="mailto:russobrendanm@gmail.com">
         <div id="contact-button" className={this.props.id}>
-          <p>Contact Me</p>
+          <p data-content="Contact Me">Contact Me</p>
           <img src={arrow} alt="Arrow to link to another page" />
         </div>
       </a>
