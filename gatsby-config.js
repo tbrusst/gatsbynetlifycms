df87276316170d984ef613113a0f4c660f74b399
BrendanRusso <russobrendanm@gmail.com>
2021-01-11 15:48:00 -0500

    seo

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     title: "Brendan Russo",
     description:
-      "This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.",
+      "Portfolio for designer and developer Brendan Russo.",
   },
   plugins: [
     "gatsby-plugin-react-helmet",
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -56,7 +56,7 @@ const TemplateWrapper = ({ children }) => {
         <meta property="og:url" content="/" />
         <meta
           property="og:image"
-          content={`${withPrefix("/")}img/og-image.jpg`}
+          content={`${withPrefix("/")}img/seo-image.jpg`}
         />
         <link
           rel="stylesheet"
