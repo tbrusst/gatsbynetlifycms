bbb48029796b8fad7e054792e1bac6f551a3a22a
Brendan Russo <russobrendanm@gmail.com>
2026-09-24 17:38:24 -0700

    Raise markdown image maxWidth to 2500 for wider body images

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,103 +1,103 @@
 module.exports = {
-  siteMetadata: {
-    title: "Brendan Russo",
-    description:
-      "Portfolio for designer and developer Brendan Russo.",
-  },
-  plugins: [
-    "gatsby-plugin-react-helmet",
-    {
-      resolve: `gatsby-plugin-scroll-reveal`,
-      options: {
-          threshold: .5, // Percentage of an element's area that needs to be visible to launch animation
-          once: true, // Defines if animation needs to be launched once
-          disable: false, // Flag for disabling animations
-          
-          // Advanced Options
-          selector: '[data-sal]', // Selector of the elements to be animated
-          animateClassName: 'sal-animate', // Class name which triggers animation
-          disabledClassName: 'sal-disabled', // Class name which defines the disabled state
-          rootMargin: '0% 50%', // Corresponds to root's bounding box margin
-          enterEventName: 'sal:in', // Enter event name
-          exitEventName: 'sal:out', // Exit event name
-      }
+    siteMetadata: {
+          title: "Brendan Russo",
+          description:
+                  "Portfolio for designer and developer Brendan Russo.",
     },
-    "gatsby-plugin-sass",
-    {
-      // keep as first gatsby-source-filesystem plugin for gatsby image support
-      resolve: "gatsby-source-filesystem",
-      options: {
-        path: `${__dirname}/static/img`,
-        name: "uploads",
+    plugins: [
+          "gatsby-plugin-react-helmet",
+      {
+              resolve: `gatsby-plugin-scroll-reveal`,
+              options: {
+                        threshold: .5, // Percentage of an element's area that needs to be visible to launch animation
+                        once: true, // Defines if animation needs to be launched once
+                        disable: false, // Flag for disabling animations
+
+                        // Advanced Options
+                        selector: '[data-sal]', // Selector of the elements to be animated
+                        animateClassName: 'sal-animate', // Class name which triggers animation
+                        disabledClassName: 'sal-disabled', // Class name which defines the disabled state
+                        rootMargin: '0% 50%', // Corresponds to root's bounding box margin
+                        enterEventName: 'sal:in', // Enter event name
+                        exitEventName: 'sal:out', // Exit event name
+              }
       },
-    },
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        path: `${__dirname}/src/pages`,
-        name: "pages",
+          "gatsby-plugin-sass",
+      {
+              // keep as first gatsby-source-filesystem plugin for gatsby image support
+            resolve: "gatsby-source-filesystem",
+              options: {
+                        path: `${__dirname}/static/img`,
+                        name: "uploads",
+              },
       },
-    },
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        path: `${__dirname}/src/img`,
-        name: "images",
+      {
+              resolve: "gatsby-source-filesystem",
+              options: {
+                        path: `${__dirname}/src/pages`,
+                        name: "pages",
+              },
       },
-    },
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-transformer-remark",
-      options: {
-        plugins: [
-          {
-            resolve: "gatsby-remark-component",
-            options: { components: ["my-component", "other-component"] },
-          },
-          {
-            resolve: `gatsby-remark-remove-root-p-tag`,
-            options: {
-              parents: ["gatsby-plugin-json-remark", "default-site-plugin"], // Optional: will process only the MarkdownRemark nodes created by these plugins
-            },
-          },
-          {
-            resolve: "gatsby-remark-relative-images",
-            options: {
-              name: "uploads",
-            },
-          },
-          {
-            resolve: "gatsby-remark-images",
-            options: {
-              // It's important to specify the maxWidth (in pixels) of
-              // the content container as this plugin uses this as the
-              // base for generating different widths of each image.
-              maxWidth: 2048,
-            },
-          },
-          {
-            resolve: "gatsby-remark-copy-linked-files",
-            options: {
-              destinationDir: "static",
-            },
-          },
+      {
+              resolve: "gatsby-source-filesystem",
+              options: {
+                        path: `${__dirname}/src/img`,
+                        name: "images",
+              },
+      },
+          "gatsby-plugin-sharp",
+          "gatsby-transformer-sharp",
+      {
+              resolve: "gatsby-transformer-remark",
+              options: {
+                        plugins: [
+                          {
+                                        resolve: "gatsby-remark-component",
+                                        options: { components: ["my-component", "other-component"] },
+                          },
+                          {
+                                        resolve: `gatsby-remark-remove-root-p-tag`,
+                                        options: {
+                                                        parents: ["gatsby-plugin-json-remark", "default-site-plugin"], // Optional: will process only the MarkdownRemark nodes created by these plugins
+                                        },
+                          },
+                          {
+                                        resolve: "gatsby-remark-relative-images",
+                                        options: {
+                                                        name: "uploads",
+                                        },
+                          },
+                          {
+                                        resolve: "gatsby-remark-images",
+                                        options: {
+                                                        // It's important to specify the maxWidth (in pixels) of
+                                          // the content container as this plugin uses this as the
+                                          // base for generating different widths of each image.
+                                          maxWidth: 2500,
+                                        },
+                          },
+                          {
+                                        resolve: "gatsby-remark-copy-linked-files",
+                                        options: {
+                                                        destinationDir: "static",
+                                        },
+                          },
+                                  ],
+              },
+      },
+      {
+              resolve: "gatsby-plugin-netlify-cms",
+              options: {
+                        modulePath: `${__dirname}/src/cms/cms.js`,
+              },
+      },
+      {
+              resolve: "gatsby-plugin-purgecss", // purges all unused/unreferenced css rules
+              options: {
+                        develop: true, // Activates purging in npm run develop
+                        purgeOnly: ["/all.sass"], // applies purging only on the bulma css file
+              },
+      }, // must be after other CSS plugins
+          "gatsby-plugin-netlify", // make sure to keep it last in the array
         ],
-      },
-    },
-    {
-      resolve: "gatsby-plugin-netlify-cms",
-      options: {
-        modulePath: `${__dirname}/src/cms/cms.js`,
-      },
-    },
-    {
-      resolve: "gatsby-plugin-purgecss", // purges all unused/unreferenced css rules
-      options: {
-        develop: true, // Activates purging in npm run develop
-        purgeOnly: ["/all.sass"], // applies purging only on the bulma css file
-      },
-    }, // must be after other CSS plugins
-    "gatsby-plugin-netlify", // make sure to keep it last in the array
-  ],
 };
